f264c648bc69ea15ef2af62f8ee4a64a321a70e9
Barasa <lizbethbarasa@gmail.com>
2023-06-19 07:52:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#iclude <stdioh>
+#include <stdio.h>
 /**
  * main -Entry point
  *
- * return:always 0(success)
+ * Return:Always 0(success)
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
